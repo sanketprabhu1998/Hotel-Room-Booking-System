4575d7cc52184141b913ca2355d14c978d76769a
sanketprabhu1998 <45327652+sanketprabhu1998@users.noreply.github.com>
2021-07-14 12:09:45 +0530

    Add files via upload

---
+++ fonts/reservationnn.php
@@ -0,0 +1,234 @@
+<?php
+include('db.php')
+?>
+<?php
+if (isset($_GET['room_id'])){
+    $get_room_id = $_GET['room_id'];
+    $get_room_sql = "SELECT * FROM room NATURAL JOIN room_type WHERE room_id = '$get_room_id'";
+    $get_room_result = mysqli_query($connection,$get_room_sql);
+    $get_room = mysqli_fetch_assoc($get_room_result);
+
+    $get_room_type_id = $get_room['room_type_id'];
+    $get_room_type = $get_room['room_type'];
+    $get_room_no = $get_room['room_no'];
+    $get_room_price = $get_room['price'];
+}
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>RESERVATION Kuthoop Hotel</title>
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a  href="index1.php"><i class="fa fa-home"></i> Homepage</a>
+                    </li>
+                    
+					</ul>
+
+            </div>
+
+        </nav>
+       
+        <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                            RESERVATION <small></small>
+                        </h1>
+                    </div>
+                </div> 
+                 
+                                 
+            <div class="row">
+                
+                <div class="col-md-6 col-sm-10">
+				<?php
+                    if (isset($_GET['room_id'])){ ?>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            PERSONAL INFORMATION
+                        </div>
+                        <div class="panel-body">
+						<form name="form" method="post">
+                            <div class="form-group">
+                                            <label>Title*</label>
+                                            <select name="title" class="form-control" required >
+												<option value selected ></option>
+                                                <option value="Dr.">Dr.</option>
+                                                <option value="Miss.">Miss.</option>
+                                                <option value="Mr.">Mr.</option>
+                                                <option value="Mrs.">Mrs.</option>
+												<option value="Prof.">Prof.</option>
+												
+                                            </select>
+                              </div>
+							  <div class="form-group">
+                                            <label>First Name</label>
+                                            <input name="fname" class="form-control" id="first_name" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Last Name</label>
+                                            <input name="lname" class="form-control" id="last_name" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Email</label>
+                                            <input name="email" type="email" class="form-control" id="email" required>
+                                            
+                               </div>
+							   <div class="form-group">
+                                            <label>Nationality*</label>
+                                            <label class="radio-inline">
+                                                <input type="radio" name="nation"  value="Indian" checked="">Indian
+                                            </label>
+                                            <label class="radio-inline">
+                                                <input type="radio" name="nation"  value="Non Indian ">Non Indian 
+                                            </label>
+                         
+                                </div>
+								<?php
+
+								$countries = array("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
+
+								?>
+								<div class="form-group">
+                                            <label>Passport Country*</label>
+                                            <select name="country" class="form-control" required>
+												<option value selected ></option>
+                                                <?php
+												foreach($countries as $key => $value):
+												echo '<option value="'.$value.'">'.$value.'</option>'; //close your tags!!
+												endforeach;
+												?>
+                                            </select>
+								</div>
+								<div class="form-group">
+                                            <label>Phone Number</label>
+                                            <input name="phone" type ="number" class="form-control" id="contact_no" required>
+                                            
+                               </div>
+								
+								
+								<div class="form-groug">	
+                                <label>ID Card Type</label>
+                                <select class="form-control" id="id_card_id" data-error="Select ID Card Types" required onchange="validId(this.value);">
+                                    <option selected disabled>Select ID Card Type</option>
+                                    <?php
+                                    $query  = "SELECT * FROM id_card_type";
+                                    $result = mysqli_query($connection,$query);
+                                    if (mysqli_num_rows($result) > 0){
+                                        while ($id_card_type = mysqli_fetch_assoc($result)){
+                                            echo '<option value="'.$id_card_type['id_card_type_id'].'">'.$id_card_type['id_card_type'].'</option>';
+                                        }}
+                                    ?>
+                                </select>
+                             </div>
+							 <div class="form-group">
+                                <label>ID Card No</label>
+                                <input type="text" class="form-control" placeholder="ID Card No" id="id_card_no" data-error="Enter Valid ID Card No" required>
+                            </div>
+							<div class="form-group">
+                                <label>Address</label>
+                                <textarea class="form-control" rows="3" id="address" required></textarea>
+                                
+                            </div>
+							   
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                  
+            <div class="row">
+                <div class="col-md-6 col-sm-10">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            RESERVATION INFORMATION
+                        </div>
+                        <div class="panel-body">
+								<div class="form-group">
+                                            <label>Type Of Room *</label>
+                                            <select name="troom"  class="form-control" id="room_type"required>
+												<option value selected ></option>
+												<option selected value="<?php echo $get_room_type_id; ?>"><?php echo $get_room_type; ?></option>
+                                            </select>
+                              </div>
+							  <!--<div class="form-group">
+                                            <label>Bedding Type</label>
+                                            <select name="bed" class="form-control" required>
+												<option value selected ></option>
+                                                <option value="Single">Single</option>
+                                                <option value="Double">Double</option>
+												<option value="Triple">Triple</option>
+                                                <option value="Quad">Quad</option>
+												<option value="None">None</option>
+                                                
+                                             
+                                            </select>
+                              </div> -->
+							  <div class="form-group">
+                                            
+                                            <label class="radio-inline">
+                                                <input type="radio" name="nation"  value="Ac" checked="">Ac
+                                            </label>
+                                            <label class="radio-inline">
+                                                <input type="radio" name="nation"  value="Non-Ac ">Non-Ac
+                                            </label>
+                         
+                                </div>
+								<div class="form-group col-lg-6">
+                                    <label>Room No</label>
+                                    <select class="form-control" id="room_no" onchange="fetch_price(this.value)" required data-error="Select Room No">
+                                        <option selected disabled>Select Room No</option>
+                                        <option selected value="<?php echo $get_room_id; ?>"><?php echo $get_room_no; ?></option>
+                                    </select>
+                                    
+                                </div>
+							  
+							 
+							 
+							  <div class="form-group">
+                                            <label>Check-In</label>
+											<input name="cin" type="text" class="form-control" placeholder="mm/dd/yyyy" id="check_in_date" data-error="Select Check In Date" required>
+                                </div>
+							   <div class="form-group">
+                                            <label>Check-Out</label>
+                                            <input name="cout" type="text" class="form-control" placeholder="mm/dd/yyyy" id="check_out_date" data-error="Select Check Out Date" required>
+                                    
+                               </div>
+							   <div>
+                                    <h4 style="font-weight: bold">Total Days : <span id="staying_day">0</span> Days</h4>
+                                    <h4 style="font-weight: bold">Price: <span id="price"><?php echo $get_room_price; ?></span> /-</h4>
+                                    <h4 style="font-weight: bold">Total Amount : <span id="total_price">0</span> /-</h4>
+                                </div>
+                       </div>
+                        
+                    </div>
+					<button type="submit" class="btn btn-lg btn-primary pull-right">Submit</button>
+                </div>
+					</form>
+							
+                    </div>
+                </div>
+            </div>
+           
+                
+                </div>
+                    
+            
+				
+					</div>
+</body>
+</html>
